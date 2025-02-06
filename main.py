b6858d066b2c40d414fa08ebcb1133406cf55365
Aguileradaniel <aguileradaniel1987@gmail.com>
2025-02-06 18:51:46 -0300

    function dict_to_list_of_dicts

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
         print(count_words(file_contents))
         charcount = count_chars(file_contents)
         print(charcount)
+        print(dict_to_list_of_dicts(charcount))
 
 def count_words(text: str) -> int:
     return len(text.split())
@@ -11,11 +12,18 @@ def count_words(text: str) -> int:
 def count_chars(text: str) -> dict:
     text = text.lower()
     resp_dict = {}
-    for char in text:
+    for char in text:        
         if char in resp_dict:
             resp_dict[char] +=1
         else:
             resp_dict[char] = 1
     return resp_dict
+
+def dict_to_list_of_dicts(dict:dict) -> list:
+    return_list = []
+    for char in list(dict):
+        if char.isalpha():
+            return_list.append({"letter":char,"count":dict[char]})
+    return return_list
 
 main()
