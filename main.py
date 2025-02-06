2a0f32b09761141c06a612f3d5800307bcf33c29
Aguileradaniel <aguileradaniel1987@gmail.com>
2025-02-06 18:18:18 -0300

    open file and count words

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(count_words(file_contents))
+
+def count_words(text: str) -> int:
+    return len(text.split())
+
+
+main()
